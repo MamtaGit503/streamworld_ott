168ff2ee203b9a13295516707c8e37f3e4637a1c
Mamta <mamta560332@gmail.com>
2025-08-01 10:46:43 +0530

    scroll

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -94,7 +94,9 @@ const Video = ({ id }) => {
           </div>
 
           {/* Right: Slider List */}
-          <div className="w-full lg:w-[400px] max-h-[500px] overflow-y-auto scrollbar-thin scrollbar-thumb-[#a70d65]/60">
+          {/* <div className="w-full lg:w-[400px] max-h-[500px] overflow-y-auto scrollbar-thin scrollbar-thumb-[#a70d65]/60"> */}
+          <div className="w-full lg:w-[400px] h-[300px] lg:h-[500px] overflow-y-auto scrollbar-thin scrollbar-thumb-[#a70d65]/60">
+
             <Swiper
               direction="vertical"
               spaceBetween={20}
